775c2e17fc34ff44f423f3e384c612ac4d7143d2
naor94 <44939883+naor94@users.noreply.github.com>
2019-06-03 14:34:15 +0300

    Update signals.c

--- signals.c
+++ signals.c
@@ -71,3 +71,104 @@
 
 // exit(0);
 // }
+
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@naor94 
+0
+0 1 Elmalem/OperatingSystem-pipe-and-fork
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Security  Insights
+OperatingSystem-pipe-and-fork/signals.c
+@Elmalem Elmalem Add files via upload
+556ee43 5 days ago
+@Elmalem @bargenish44
+82 lines (65 sloc)  1.69 KB
+    
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<string.h>
+#include<wait.h>
+#include<signal.h>
+#include<sys/wait.h>
+
+
+
+// my signals :: every signal is probably int value
+#define SIGREADY 1
+#define SIGCAUGHT 2
+#define SIGDEAD 3
+
+
+
+void signal_handle(int signal){
+    if(signal == SIGREADY ) printf("PID %d %s\n",getpid(),"ready");
+    else if(signal == SIGCAUGHT ) printf("PID %d %s\n",getpid(),"caught one");
+    else if(signal ==  SIGDEAD ) printf("PID %d %s\n",getpid(),"dead");
+}
+
+int main(){
+// to create 5 process (include the source process), i actually create 4 childs
+// for the 4 childrens create , i need to use in 2 times in fork().
+// 2^x = childrens , when x is fork() calls.
+
+pid_t pid; // temp value for fork function answer
+
+pid_t zombies[5];
+
+// create 4 process :: TOTAL 5 (father process include that)
+for (size_t i = 0; i < 4; i++) {
+  pid = fork();
+  if(!pid)
+    break;
+}
+
+
+for (size_t i = 0; i < 5; i++) {
+  if (signal(SIGREADY, signal_handle) == SIG_ERR){
+  printf("%s\n","can't catch SIGREADY");
+  }
+  else{
+  sleep(1);
+  kill(getpid(),SIGREADY); // kill and catch the signal
+  break;
+  }
+}
+
+for (size_t i = 0; i < 5; i++) {
+  if (signal(SIGCAUGHT, signal_handle) == SIG_ERR){
+  printf("%s\n","can't catch SIGREADY");
+  }
+  else{
+  sleep(1);
+  kill(getpid(),SIGCAUGHT); // kill and catch the signal
+  break;
+  }
+}
+
+
+pid_t temp; // for the zombies catch
+
+for (size_t i = 0; i < 5; i++) {
+  if (signal(SIGDEAD, signal_handle) == SIG_ERR){
+  printf("%s\n","can't catch SIGREADY");
+  }
+  else{
+  sleep(1);
+  temp = getpid();
+  kill(temp,SIGDEAD); // kill and catch the signal
+  zombies[i] = temp; // father catch all the zombies
+  break;
+  }
+}
+exit(0);
+}
