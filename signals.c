aaf4f957b2c3f89f86cdd34023bf0ad362e37e96
naor94 <44939883+naor94@users.noreply.github.com>
2019-06-03 14:31:57 +0300

    Update signals.c

--- signals.c
+++ signals.c
@@ -1,73 +1,73 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<string.h>
-#include<wait.h>
-#include<signal.h>
-#include<sys/wait.h>
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<stdio.h>
+// #include<unistd.h>
+// #include<sys/types.h>
+// #include<string.h>
+// #include<wait.h>
+// #include<signal.h>
+// #include<sys/wait.h>
 
 
-void signal_handle1(int signal){
-    printf("PID %d %s\n",getpid(),"ready");}
-void signal_handle2(int signal){
-    printf("PID %d %s\n",getpid(),"caught");}
-void signal_handle3(int signal){
-    printf("PID %d %s\n",getpid(),"dead");}
+// void signal_handle1(int signal){
+//     printf("PID %d %s\n",getpid(),"ready");}
+// void signal_handle2(int signal){
+//     printf("PID %d %s\n",getpid(),"caught");}
+// void signal_handle3(int signal){
+//     printf("PID %d %s\n",getpid(),"dead");}
 
-int main (){
+// int main (){
 
-pid_t pid;
+// pid_t pid;
 
-// the zombie collector
+// // the zombie collector
 
-pid_t zombies[5];
+// pid_t zombies[5];
 
-// create 4 more processes
-for (int i = 0; i < 4; i++) {
-  pid = fork();
-  if(!pid)
-    break;
-}
+// // create 4 more processes
+// for (int i = 0; i < 4; i++) {
+//   pid = fork();
+//   if(!pid)
+//     break;
+// }
 
-    printf("out the loop\n");
+//     printf("out the loop\n");
 
-// signal check
-  if (signal(1, signal_handle1) == SIG_ERR){
-  printf("%s\n","can't catch SIGREADY");
-  }
-  else{
-//   wait(0);
-  sleep(2);
-  kill(getpid(),1); // kill order - and set red
-  }
-
-
-
-  if (signal(1, signal_handle2) == SIG_ERR){
-  printf("%s\n","can't catch SIGREADY");
-  }
-  else{
-  sleep(2);
-  kill(getpid(),1); // kill and catch the signal
-  }
+// // signal check
+//   if (signal(1, signal_handle1) == SIG_ERR){
+//   printf("%s\n","can't catch SIGREADY");
+//   }
+//   else{
+// //   wait(0);
+//   sleep(2);
+//   kill(getpid(),1); // kill order - and set red
+//   }
 
 
 
-pid_t temp; // for the zombies catch
-int i=0;
-  if (signal(1, signal_handle3) == SIG_ERR){
-  printf("%s\n","can't catch SIGREADY");
-  }
-  else{
-  sleep(2);
-  temp = getpid();
-  kill(getpid(),1); // kill and catch the signal
-  zombies[i] = getpid(); // father catch all the zombies
-  i++;
-  }
+//   if (signal(1, signal_handle2) == SIG_ERR){
+//   printf("%s\n","can't catch SIGREADY");
+//   }
+//   else{
+//   sleep(2);
+//   kill(getpid(),1); // kill and catch the signal
+//   }
 
 
-exit(0);
-}
+
+// pid_t temp; // for the zombies catch
+// int i=0;
+//   if (signal(1, signal_handle3) == SIG_ERR){
+//   printf("%s\n","can't catch SIGREADY");
+//   }
+//   else{
+//   sleep(2);
+//   temp = getpid();
+//   kill(getpid(),1); // kill and catch the signal
+//   zombies[i] = getpid(); // father catch all the zombies
+//   i++;
+//   }
+
+
+// exit(0);
+// }
